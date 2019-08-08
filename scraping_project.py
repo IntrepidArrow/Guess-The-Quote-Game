488e0481af38506aa2deae4a272e93abe3ccb961
Abhimukth Chaudhuri <abhimukth.chaudhuri@mail.mcgill.ca>
2019-08-08 18:15:30 -0400

    Get all quote information for game from page 1

--- scraping_project.py
+++ scraping_project.py
@@ -2,14 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 
 # http://quotes.toscrape.com
-url = "http://quotes.toscrape.com/page/1/"
+url = "http://quotes.toscrape.com/"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 
-quote = soup.find(class_="quote")
-text = quote.find(class_="text").get_text()
-author = quote.find(class_="author").get_text()
-href = quote.find("a")["href"]
-print(text)
-print(author)
-print(href)
+# Getting all Quotes from current page and storing in a list
+quotes = soup.find_all(class_="quote")
+game_data = []
+for quote in quotes:
+    text = quote.find(class_="text").get_text()
+    author = quote.find(class_="author").get_text()
+    href = quote.find("a")["href"]
+    
+    author_data = [text, author, href]
+    # Adding scraped information in list for game logic
+    game_data.append(author_data)
